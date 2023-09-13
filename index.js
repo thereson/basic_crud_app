a1080b7333b0c91c1be9a6026e9a20b14725363c
thereson <gerardokolie09@gmail.com>
2023-09-13 04:07:49 -0700

    iimplememted mvc approach

--- index.js
+++ index.js
@@ -1,82 +1,22 @@
 let express = require("express");
 const mongoose = require("mongoose");
 let PORT = 8080;
-let model = require("./models/model");
+// let model = require("./models/model");
+
+let user_route = require("./routes/userRoute");
 let ec2_url = "mongodb://54.221.51.134:27017";
-// let mongo_url = "mongodb://127.0.0.1:27017";
+let mongo_url = "mongodb://127.0.0.1:27017";
 let connect_mongodb = async (url) => {
   let conn = await mongoose.connect(url, { dbName: "api_test" });
   console.log("successfully connected to mongodb");
 };
-connect_mongodb(ec2_url);
+connect_mongodb(mongo_url);
 
 let server = express();
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-server.post("/api", async (req, res) => {
-  try {
-    if (JSON.stringify(req.body) === "{}") {
-      throw new Error("invalid request body");
-    } else {
-      let user = new model({ name: req.body.name });
-      await user.save();
-      res.status(201).send(user);
-    }
-  } catch (err) {
-    res.status(400).send(err.message);
-  }
-});
 
-server.get("/api/:user_id", async (req, res) => {
-  let id = req.params.user_id;
-  let user = await model.find({ _id: id });
-  try {
-    if (user.length === 0) {
-      throw new Error(`id ${id} not found`);
-    }
-    res.status(200).json(user);
-  } catch (err) {
-    res.status(404).send(err.message);
-  }
-});
-server.patch("/api/:user_id", async (req, res) => {
-  let id = req.params.user_id;
-  let { name } = req.body;
-  try {
-    let user = await model.find({ _id: id });
-    if (user.length == 0) {
-      throw new Error(`invalid id ${id}`);
-    }
-    await model.updateOne({ _id: id }, { $set: { name: name } });
-    let responce = {
-      message: "user updates",
-      id: user._id,
-      new_name: name,
-    };
-    res.status(200).send(user);
-  } catch (err) {
-    res.status(404).send(err.message);
-  }
-});
-
-server.delete("/api/:user_id", async (req, res) => {
-  let id = req.params.user_id;
-  try {
-    let user = await model.find({ _id: id });
-    if (user.length == 0) {
-      throw new Error(`invalid id ${id}`);
-    }
-    await model.deleteOne({ _id: id });
-    let responce = {
-      message: "user deleted",
-      id: user._id,
-      name: user.name,
-    };
-    res.status(200).json(responce);
-  } catch (err) {
-    res.status(404).send(err.message);
-  }
-});
+server.use(user_route);
 
 server.listen(8080, () => {
   console.log(`server listening on port ${PORT}`);
---
+++ routes/userRoute.js
@@ -0,0 +1,73 @@
+const express = require("express");
+
+let model = require("../models/model");
+
+const router = express.Router();
+
+router.post("/api", async (req, res) => {
+  try {
+    if (JSON.stringify(req.body) === "{}") {
+      throw new Error("invalid request body");
+    } else {
+      let user = new model({ name: req.body.name });
+      await user.save();
+      res.status(201).send(user);
+    }
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+router.get("/api/:user_id", async (req, res) => {
+  let id = req.params.user_id;
+  let user = await model.find({ _id: id });
+  try {
+    if (user.length === 0) {
+      throw new Error(`id ${id} not found`);
+    }
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(404).send(err.message);
+  }
+});
+
+router.patch("/api/:user_id", async (req, res) => {
+  let id = req.params.user_id;
+  let { name } = req.body;
+  try {
+    let user = await model.find({ _id: id });
+    if (user.length == 0) {
+      throw new Error(`invalid id ${id}`);
+    }
+    await model.updateOne({ _id: id }, { $set: { name: name } });
+    let responce = {
+      message: "user updates",
+      id: user._id,
+      new_name: req.body.name,
+    };
+    res.status(200).send(user);
+  } catch (err) {
+    res.status(404).send(err.message);
+  }
+});
+
+router.delete("/api/:user_id", async (req, res) => {
+  let id = req.params.user_id;
+  try {
+    let user = await model.find({ _id: id });
+    if (user.length == 0) {
+      throw new Error(`invalid id ${id}`);
+    }
+    await model.deleteOne({ _id: id });
+    let responce = {
+      message: "user deleted",
+      id: user._id,
+      name: user.name,
+    };
+    res.status(200).json(responce);
+  } catch (err) {
+    res.status(404).send(err.message);
+  }
+});
+
+module.exports = router;
